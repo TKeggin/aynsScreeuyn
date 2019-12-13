6dce493555b83a36f8086bb4328eb87ce712caf4
Thomas <thomaskeggin@hotmail.com>
2019-12-13 12:04:51 +0100

    gasmPlotOutputs
    Set up to plot species richness for a gasm run.

--- gasm/gasmPlotOutputs.R
+++ gasm/gasmPlotOutputs.R
@@ -1,55 +1,82 @@
 # set session ####
 
-setwd("C:/Users/thoma/Desktop/gasm/output/6d/my_sample_config")
+setwd("C:/Users/thoma/Desktop/gasm/output/6d/test5")
 
 library(ape)
 library(tidyverse)
 
-# load data ####
+# explore ####
 
 phy     <- read.nexus("./phy.nex")
-species <- readRDS("./species/species_t_0.rds")
 land    <- readRDS("./landscapes/landscape_t_0.rds")
+species <- readRDS("./species/species_t_0.rds")
 
-# phylogeny ####
-# plot
+# phylogeny
 plot(phy)
 
-# species ####
-# explore
+# species
 labels(species)
 species[[1]]$id
 species[[1]]$abundance
 species[[1]]$traits
 
-# landscapes ####
-# explore
+# landscapes
 land$id
 land$environment
 land$coordinates
 
-# plot ####
+# plot species richness ####
 
-# merge coordinate and abundance data for all species
-spp <- seq(1, length(species))
+# create plots directory
+dir.create("./plots")
 
-distribution <- rownames_to_column(data.frame(land$coordinates))
+# read and quantify timesteps
+timesteps.file <- list.files("./species/")
+timesteps.seq  <- seq(0,length(timesteps.file)-1)
 
-for(i in spp){
-  sp <- species[[i]]$abundance
-  sp[!is.na(sp)] <- 1
-  sp <- rownames_to_column(data.frame(sp))
+for(t in timesteps.seq){
+
+  # load data
+  species <- readRDS(paste0("./species/species_t_",t,".rds", sep = ""))
+  land    <- readRDS(paste0("./landscapes/landscape_t_",t,".rds", sep = ""))
   
-  distribution <- left_join(distribution,sp, by = "rowname")
+  # find number of species present
+  spp <- seq(1, length(species))
+  
+  # create a dataframe in which to put species distributions
+  distribution <- rownames_to_column(data.frame(land$coordinates))
+  
+  # put all species abundances into distribution dataframe and convert to binary presence/absence
+  for(i in spp){
+    sp <- species[[i]]$abundance
+    sp[!is.na(sp)] <- 1
+    sp <- rownames_to_column(data.frame(sp))
+    
+    distribution <- left_join(distribution,sp, by = "rowname")
+  }
+  colnames(distribution) <- c("cell","x","y",spp)
+  
+  # calculate species richness
+  if(dim(distribution)[2]>4){
+    richness <- rowSums(!is.na(distribution[,-c(1:3)]))
+  } else {
+    richness <- distribution[,4]
+  }
+  distribution <- cbind(distribution,richness)
+  
+  # plot and print species richness
+  jpeg(file.path("./plots", paste0(sprintf("%04i",t) ,".jpg")), width = 680, height = 480)
+    print(ggplot(data = distribution, aes(x=x,y=y)) +
+            geom_tile(aes(fill = richness), colour = "lightgrey") +
+            scale_fill_gradientn(colours = rev(terrain.colors(10)), limits = c(0,55)) +
+            xlim(c(-180,180)) +
+            ylim(c(-90,90)) +
+            ggtitle(paste0(sprintf("%04i",t))) +
+            coord_fixed() +
+            theme_light()
+    )
+  dev.off()
+  
+  print(paste(t, "complete"))
+  
 }
-colnames(distribution) <- c("cell","x","y",spp)
-
-richness <- rowSums(!is.na(distribution[,-c(1:3)]))
-
-distribution <- cbind(distribution,richness)
-
-
-
-ggplot(data = distribution, aes(x=x,y=y, fill = richness)) +
-  geom_tile()
-
