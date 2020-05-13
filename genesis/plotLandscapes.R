70bbc8a9d256cdb03acaf3a170584b95a5cdc5fd
Thomas <thomaskeggin@hotmail.com>
2020-05-13 13:07:23 +0200

    plotLandscapes improvements

--- genesis/plotLandscapes.R
+++ genesis/plotLandscapes.R
@@ -1,44 +1,46 @@
-setwd("D:/genesis/input/1d_2000m")
+setwd("D:/genesis/input/1d_all")
 
 library(tidyverse)
 library(gridExtra)
 
 landscapes <- readRDS("./landscapes.rds")
 
-# setwd("./plots")
+setwd("./plot")
 
-for(i in seq(3,6)){
-  
-  step <- i
+for(step in seq(3,1203)){
   
   tempFill  <- landscapes$temp[,step]
   depthFill <- landscapes$depth[,step]
   
-  plotTemp <- ggplot(temp, aes(x = x, y = y, fill = tempFill)) +
+  plotTemp <- ggplot(landscapes$temp, aes(x = x, y = y, fill = tempFill)) +
     geom_tile() +
-    ggtitle(paste(colnames(temp[step]),"mya")) +
-    scale_fill_gradient2(low = "blue",
-                         mid = "yellow",
-                         high = "red",
-                         na.value = "white",
-                         limits = c(20,40)) +
+    ggtitle(paste(colnames(landscapes$temp[step]),"mya")) +
+    scale_fill_gradient2(low = "darkblue",
+                         #mid = "grey",
+                         high = "darkred",
+                         na.value = "grey30",
+                         midpoint = 0,
+                         limits = c(-41,30)) +
     xlim(-180,180) +
     ylim(-90,90) +
     coord_fixed() +
     theme_void()
   
   
-  plotDepth <- ggplot(depth, aes(x = x, y = y, fill = depthFill)) +
+  plotDepth <- ggplot(landscapes$depth, aes(x = x, y = y, fill = depthFill)) +
     geom_tile() +
-    scale_fill_gradient(low = "black",
-                          high = "lightblue",
-                          na.value = "white") +
+    scale_fill_gradient(low = "darkblue",
+                        high = "white",
+                        na.value = "grey30",
+                        limits = c(-7610.05,0)) +
     xlim(-180,180) +
     ylim(-90,90) +
     coord_fixed() +
     theme_void()
   
-  png(filename=paste0(i,".png"))
+  png(filename=paste0(step-3,".png"))
   grid.arrange(plotTemp, plotDepth, nrow = 2)
   dev.off()
+  
+  print(paste(step-3," done"))
 }
